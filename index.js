5a0888d042bbd8a6dc84a9ebc72d9c2940c6fae2
Josh <gillingja@gmail.com>
2019-01-31 14:47:59 +1000

    google oauth setup

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express'); // Using require because Node doesn't support ES6 import statements yet
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ seeyalater: 'aligator' });
-});
+passport.use(new GoogleStrategy({
+    clientID: keys.GOOGLE_CLIENT_ID,
+    clientSecret: keys.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
+
+// GoogleStrategy is automatically referenced with 'google' within passport
+// additionally 'profile', and 'email' are pre-defined identifiers for information within the Google+ API
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
 // Express app to listen on dynamically assigned port
 const PORT = process.env.PORT || 5000;
