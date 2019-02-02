dc212966271193faefb9a921d46c60c1445b8693
Josh <gillingja@gmail.com>
2019-02-02 11:35:50 +1000

    CookieSession used to create secure cookies for user identification

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express'); // Using require because Node doesn't support ES6 import statements yet
 const mongoose = require('mongoose');
-
-require('./models/User');
-require('./services/passport'); // Not being referenced in this file, just executed
+const cookieSession = require('cookie-session'); // Does all the cookie creation and encryption
+const passport = require('passport');
 
 const authRoutes = require('./routes/authRoutes');
 const keys = require('./config/keys');
 
+require('./models/User');
+require('./services/passport'); // Not being referenced in this file, just executed
+
 mongoose.connect(keys.MONGO_URI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // How long the cookie will last (in milliseconds)
+    keys: [keys.COOKIE_KEY] // An array of keys from which one will be randomly selected by cookieSession to encrypt the data
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,8 @@ module.exports = (app) => {
   // Passport automatically recognises there is a code in the URL, meaning authentication has already happened...
   // ... and takes this code, putting it into the accessToken variable
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
