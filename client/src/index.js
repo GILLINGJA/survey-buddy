e63d620a0ec4e70ad7d59a702852cdb8f199e918
Josh <gillingja@gmail.com>
2019-05-24 12:06:51 +1000

    MaterializeCSS + first action-reducer flow to check if User logged in

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import 'materialize-css/dist/css/materialize.min.css';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -11,7 +14,7 @@ import reducers from './reducers';
 // Arg 2 - initial state
 // Arg 3 - middleware to apply to actions passed through reducers
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
