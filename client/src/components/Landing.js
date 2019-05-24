f40c5d349263122173e1d9c085453d2b0c39bfc5
Josh <gillingja@gmail.com>
2019-05-24 13:10:23 +1000

    Logout redirect set up + Intro to react-router's Link tag to create conditional logo link

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>SurveyBuddy!</h1>
+      <p>Collect feedback from your users in a fun and creative way!</p>
+    </div>
+  );
+}
+
+export default Landing;
